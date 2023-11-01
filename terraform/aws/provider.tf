362669f9e36ee75010f688dc532a69cc2acb2407
FischerGuti <vfischer634@gmail.com>
2023-11-01 20:37:00 -0300

    pfsenho

--- terraform/aws/provider.tf
+++ terraform/aws/provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
    backend "s3" {
-    bucket         = "fishinho"
+    bucket         = "coringuinhavaiperder232"
     key            = "terraform.tfstate"
     dynamodb_table = "fishinho"
     region         = "us-east-1"
